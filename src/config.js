340cb93b4046ed696f948d7e406f1fa458b16bc8
Dima <example.gmail.com>
2025-11-10 20:42:47 +0300

    finish config

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
+//does not work
 export const config = {
     Api_Key: '4aa432b6671f1c7772bbfc0627ecc65c',
     Api_Url: 'http://ws.audioscrobbler.com/2.0/'
--- src/hooks/useSearch.js
+++ src/hooks/useSearch.js
@@ -1,4 +1,4 @@
-import { config } from "../config";
+import { config } from "../config-api";
 import { useEffect, useState } from "react"
 
 
