84097e0389454a71855b17503a648810c6a3491e
Dima <example.gmail.com>
2025-12-03 23:05:19 +0300

    Added api request processing

--- src/config-api.js
+++ src/config-api.js
@@ -1,7 +1,3 @@
-/* export const config = {
-    Api_Key: '9c216269c789c83fcec15df88b0e0688',
-    Api_Url: 'http://ws.audioscrobbler.com/2.0/'
-} */
 const API_KEYS = {
     API_KEY: import.meta.env.VITE_API_KEY,
     API_URL: import.meta.env.VITE_API_URL,
--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+﻿import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
@@ -8,4 +8,15 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  server: {
+    proxy: {
+      // Это правило говорит Vite: все запросы, начинающиеся с /api/lastfm,
+      // перенаправляй на целевой сервер Last.fm
+      '/api/lastfm': {
+        target: 'http://ws.audioscrobbler.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/lastfm/, '/2.0')
+      }
+    }
+  }
 })
