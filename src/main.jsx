d04156055f6bedb7a98937233ae6160a1e0f0adc
Dima <example.gmail.com>
2025-12-04 19:07:16 +0300

    add hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { BrowserRouter } from 'react-router'
+import { BrowserRouter, HashRouter } from 'react-router'
 import App from './App.jsx'
 
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <StrictMode>
       <App />
     </StrictMode>
-  </BrowserRouter>
+  </HashRouter>
 )
